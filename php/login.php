53c66f70f4a4f19b65b455c4035f1dd730c2058f
sumatege <sumate.ge@gmail.com>
2021-08-28 07:42:41 +0700

    change

--- php/login.php
+++ php/login.php
@@ -3,9 +3,9 @@
 session_start();
 require 'config.php';
 
-if (isset($_POST["phone"]) && isset($_POST["pass"])) {
-    $user = $_POST["phone"];
-    $pw = $_POST["pass"];
+if (isset($_GET["phone"]) && isset($_GET["pass"])) {
+    $user = $_GET["phone"];
+    $pw = $_GET["pass"];
 
     $sql = "SELECT * FROM member WHERE m_phone='" . $user . "' AND m_password='" . $pw . "'";
     $result = $conn->query($sql);
