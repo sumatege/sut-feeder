64dc6b66e1673190933029b2b08d29b7dddd6390
Sumate Gesornchuen <46530532+sumatege@users.noreply.github.com>
2021-09-03 10:58:51 +0700

    Add files via upload

---
+++ php/machine-get-automation.php
@@ -0,0 +1,28 @@
+<?php
+
+$servername = "remotemysql.com";
+$username = "YOVzbEn8XW";
+$password = "VBA5JgCdGV";
+$db = "YOVzbEn8XW";
+
+$conn = new mysqli($servername, $username, $password, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM automation WHERE a_project_key='" . $_GET["p_key"] . "'";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+    echo json_encode($data);
+} else {
+    echo "error";
+}
+
+
+$conn->close();
