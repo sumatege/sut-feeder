7fbd8970317bc795a972df4ada15ca8485129c04
sumatege <sumate.ge@gmail.com>
2021-09-04 22:13:03 +0700

    change

--- php/config.php
+++ php/config.php
@@ -5,16 +5,18 @@ $password = "";
 $db = "feeder";
 */
 
-/*$servername = "157.245.149.179";
-$username = "io";
-$password = "7777777777yU";
-$db = "feeder";
-*/
-
+/*
 $servername = "remotemysql.com";
 $username = "YOVzbEn8XW";
 $password = "VBA5JgCdGV";
 $db = "YOVzbEn8XW";
+*/
+
+$servername = "http://159.89.204.217/";
+$username = "root";
+$password = "2021@Password";
+$db = "sut-feeder";
+
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $db);
