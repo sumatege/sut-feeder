63a1111ac65a8d1ea6b53ee658d9d15f92e022d6
sumatege <sumate.ge@gmail.com>
2021-09-04 22:27:14 +0700

    change

--- php/config.php
+++ php/config.php
@@ -5,17 +5,19 @@ $password = "";
 $db = "feeder";
 */
 
-/*
+
 $servername = "remotemysql.com";
 $username = "YOVzbEn8XW";
 $password = "VBA5JgCdGV";
 $db = "YOVzbEn8XW";
-*/
 
+
+/*
 $servername = "159.89.204.217";
 $username = "root";
 $password = "2021@Password";
 $db = "sut-feeder";
+*/
 
 
 // Create connection
