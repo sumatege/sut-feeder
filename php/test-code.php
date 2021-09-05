b629638af2489b2fddcc936589becc151d3b4959
sumatege <sumate.ge@gmail.com>
2021-09-05 13:51:55 +0700

    change

--- php/test-code.php
+++ php/test-code.php
@@ -1,3 +1,17 @@
 <?php
 
-echo "1";
+$servername = "159.89.204.217";
+$username = "root";
+$password = "2021@Password";
+$db = "sut-feeder";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}else{
+    echo "success";
+}
