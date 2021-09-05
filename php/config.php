26aa9b3b051de0afb04be5666db349de7407731a
sumatege <sumate.ge@gmail.com>
2021-09-05 15:01:49 +0700

    change

--- php/config.php
+++ php/config.php
@@ -5,19 +5,17 @@ $password = "";
 $db = "feeder";
 */
 
-
+/*
 $servername = "remotemysql.com";
 $username = "YOVzbEn8XW";
 $password = "VBA5JgCdGV";
 $db = "YOVzbEn8XW";
+*/
 
-
-/*
-$servername = "159.89.204.217";
-$username = "root";
+$servername = "127.0.0.1";
+$username = "sut-feeder";
 $password = "2021@Password";
 $db = "sut-feeder";
-*/
 
 
 // Create connection
