32b7630e902fcb857ffa10586c6f0d5fcfe78eaa
sumatege <sumate.ge@gmail.com>
2021-09-06 14:24:14 +0700

    change

--- php/config.php
+++ php/config.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
+/*$servername = "localhost";
 $username = "root";
 $password = "";
-$db = "feeder";
+$db = "feeder";*/
 
 
 /*
@@ -12,12 +12,12 @@ $password = "VBA5JgCdGV";
 $db = "YOVzbEn8XW";
 */
 
-/*
+
 $servername = "127.0.0.1";
 $username = "sut-feeder";
 $password = "2021@Password";
 $db = "sut-feeder";
-*/
+
 
 
 // Create connection
