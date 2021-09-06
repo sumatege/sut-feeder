3b863540af1fa95f86b23b2d1d5a3d616c1041c6
sumatege <sumate.ge@gmail.com>
2021-09-07 01:29:16 +0700

    change

--- php/save-restart-stop-automation.php
+++ php/save-restart-stop-automation.php
@@ -4,7 +4,7 @@ session_start();
 require 'config.php';
 
 $sql = "UPDATE project SET    
-    p_weather_status = " . $_GET["status"] . ", 
+    p_weather_status = '" . $_GET["status"] . "', 
     p_weather_start_time = '" . $_GET["date"] . "'
     WHERE p_key='" . $_SESSION["selectedKey"] . "'";
 
