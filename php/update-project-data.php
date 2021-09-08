da10de4159514f2c572a6b28436d5d75578780af
sumatege <sumate.ge@gmail.com>
2021-09-08 21:46:08 +0700

    change

--- php/update-project-data.php
+++ php/update-project-data.php
@@ -6,13 +6,13 @@ require 'config.php';
 if($_GET["sdate"] != null && $_GET["edate"] != null && $_GET["bweight"] != null && $_GET["bamount"] != null && $_GET["eweight"] != null){
     $sql = "UPDATE project SET
     p_start_date='" . $_GET["sdate"] . "',
-    p_end_date='" . "" . "',
+    p_end_date='" . $_GET["edate"] . "',
     p_fish_amount='" . $_GET["bamount"] . "',
     p_fish_begin_weight='" . $_GET["bweight"] . "',
     p_fish_end_weight='" . $_GET["eweight"] . "'
     WHERE p_key='" . $_SESSION["selectedKey"] . "'";
 
-    if ($conn->query($sql) === TRUE)         
+    if ($conn->query($sql) === TRUE) {
         echo "0";
         echo $sql;
     } else {
