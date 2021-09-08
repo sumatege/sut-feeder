1087a8c0b8f7cfd22c7b5f5423d8351552b6a628
sumatege <sumate.ge@gmail.com>
2021-09-08 22:06:54 +0700

    change

--- php/update-project-data.php
+++ php/update-project-data.php
@@ -3,23 +3,31 @@
 session_start();
 require 'config.php';
 
-if($_GET["sdate"] != null && $_GET["edate"] != null && $_GET["bweight"] != null && $_GET["bamount"] != null && $_GET["eweight"] != null){
-    $sql = "UPDATE project SET
-    p_start_date=" . strtotime($_GET["sdate"]) . ",
-    p_end_date=" . strtotime($_GET["edate"]) . ",
+if ($_GET["sdate"] == null) {
+    $sdate = null;
+} else {
+    $sdate = "'" . $_GET["sdate"] . "'";
+}
+
+if ($_GET["edate"] == null) {
+    $edate = null;
+} else {
+    $edate = "'" . $_GET["sdate"] . "'";
+}
+
+$sql = "UPDATE project SET
+    p_start_date=" . $sdate . ",
+    p_end_date=" . $edate . ",
     p_fish_amount='" . $_GET["bamount"] . "',
     p_fish_begin_weight='" . $_GET["bweight"] . "',
     p_fish_end_weight='" . $_GET["eweight"] . "'
     WHERE p_key='" . $_SESSION["selectedKey"] . "'";
 
-    echo $sql . "<br/>";
-    if ($conn->query($sql) === TRUE) {
-        echo "0";
-    } else {
-        echo "Error updating record: " . $conn->error;
-    }
-}else{
-    echo "1";
+echo $sql;
+if ($conn->query($sql) === TRUE) {
+    echo "0";
+} else {
+    echo "Error updating record: " . $conn->error;
 }
 
 $conn->close();
