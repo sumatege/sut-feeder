a4273f23519e7a476c21ac3294ef62731b3f8f36
sumatege <sumate.ge@gmail.com>
2021-09-17 11:29:30 +0700

    change

--- php/machine-log-data.php
+++ php/machine-log-data.php
@@ -2,8 +2,8 @@
 
 require 'config.php';
 
-$sql = "INSERT INTO log_data (p_key,food_status,sollar_cell_voltage,battery_voltage,mcu_temp) VALUES
-('" . $_GET["p_key"] . "'," . $_GET["food_status"] . "," . $_GET["sollar_cell_voltage"] . "," . $_GET["battery_voltage"] . "," . $_GET["mcu_temp"] . ")";
+$sql = "INSERT INTO log_data (p_key,food_status,sollar_cell_voltage,battery_voltage,mcu_temp,timestamp) VALUES
+('" . $_GET["p_key"] . "'," . $_GET["food_status"] . "," . $_GET["sollar_cell_voltage"] . "," . $_GET["battery_voltage"] . "," . $_GET["mcu_temp"] . "," . $_GET["timestamp"] . ")";
 
 if ($conn->query($sql) === TRUE) {
     echo "0";
