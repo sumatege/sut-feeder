dd9fbe4ddfd5ca54a0febda5f485b2c4dff99bbd
sumatege <sumate.ge@gmail.com>
2021-09-18 14:35:43 +0700

    change

--- php/machine-log-data.php
+++ php/machine-log-data.php
@@ -2,8 +2,10 @@
 
 date_default_timezone_set('Asia/Bangkok');
 $newTime = date("Y-m-d H:i:s", strtotime(date("Y-m-d H:i:s") . " +7 hours"));
-require 'config.php';
 echo $date("Y-m-d H:i:s");
+
+require 'config.php';
+
 $sql = "INSERT INTO log_data (p_key,food_status,sollar_cell_voltage,battery_voltage,mcu_temp,timestamp) VALUES
 ('" . $_GET["p_key"] . "'," . $_GET["food_status"] . "," . $_GET["sollar_cell_voltage"] . "," . $_GET["battery_voltage"] . "," . $_GET["mcu_temp"] . ",'" . date("Y-m-d H:i:s") . "')";
 
