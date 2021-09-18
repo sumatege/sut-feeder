020143d2dfe2791365085cc87e950cbdc762484f
sumatege <sumate.ge@gmail.com>
2021-09-18 14:36:51 +0700

    change

--- php/machine-log-data.php
+++ php/machine-log-data.php
@@ -1,11 +1,11 @@
 <?php
 
-
+$newTime = date("Y-m-d H:i:s", strtotime(date("Y-m-d H:i:s") . " +7 hours"));
 
 require 'config.php';
 
 $sql = "INSERT INTO log_data (p_key,food_status,sollar_cell_voltage,battery_voltage,mcu_temp,timestamp) VALUES
-('" . $_GET["p_key"] . "'," . $_GET["food_status"] . "," . $_GET["sollar_cell_voltage"] . "," . $_GET["battery_voltage"] . "," . $_GET["mcu_temp"] . ",'" . date("Y-m-d H:i:s") . "')";
+('" . $_GET["p_key"] . "'," . $_GET["food_status"] . "," . $_GET["sollar_cell_voltage"] . "," . $_GET["battery_voltage"] . "," . $_GET["mcu_temp"] . ",'" . $newTime . "')";
 
 if ($conn->query($sql) === TRUE) {
     echo "0";
