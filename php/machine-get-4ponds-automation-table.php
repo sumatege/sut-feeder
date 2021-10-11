686533ae3bf1b742522174918dcbd145e9168b2f
sumatege <sumate.ge@gmail.com>
2021-10-11 21:05:23 +0700

    change

--- php/machine-get-4ponds-automation-table.php
+++ php/machine-get-4ponds-automation-table.php
@@ -10,7 +10,7 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        $data = $row;
+        $data[] = $row;
     }
     echo json_encode($data);
 } else {
